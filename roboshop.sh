fe5b2650bb33414bfaf39394e9fd95e679c01374
Ravi <ravi.k@codonsystems.com>
2026-02-26 17:37:45 -0500

    actions

--- roboshop.sh
+++ roboshop.sh
@@ -16,12 +16,12 @@ do
 
 if [ "$instance" = "frontend" ]; then
     IP=$(aws ec2 describe-instances \
-        --instance-ids "$INSTANCE_ID" \
+        --instance-ids $INSTANCE_ID \
         --query 'Reservations[0].Instances[0].PublicIpAddress' \
         --output text)
 else
     IP=$(aws ec2 describe-instances \
-        --instance-ids "$INSTANCE_ID" \
+        --instance-ids $INSTANCE_ID \
         --query 'Reservations[0].Instances[0].PrivateIpAddress' \
         --output text)
 fi
