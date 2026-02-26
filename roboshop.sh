bb7329b92a2eaeda1c5fbff8cc8458bf97fe5104
Ravi <ravi.k@codonsystems.com>
2026-02-26 17:30:30 -0500

    actions

--- roboshop.sh
+++ roboshop.sh
@@ -2,34 +2,30 @@
 
 SG_ID="sg-048193ac291fc884d"
 AMI_ID="ami-0220d79f3f480ecf5"
-INSTANCE_TYPE="t3.micro"
-
+# INSTANCE_TYPE="t3.micro"
 
 for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances \
-    --image-id $AMI_ID \        
-    '--instance-type' $INSTANCE_TYPE \
-    -- security-group-ids $SG_ID \
-    --tag-specifications ResourceType=instance,Tags=[{Key=Name,Value=$instance}] \      
-    '--query Instances[0].InstanceId' \
-    --output text)
+        --image-id $AMI_ID \
+        --instance-type "t3.micro" \
+        --security-group-ids $SG_ID \
+        --tag-specifications ResourceType=instance,Tags=[{Key=Name,Value=$instance}] \
+        --query 'Instances[0].InstanceId' \
+        --output text)
 
-    if [ $instance == "frontend" ]; then
-        IP=$(
-            aws ec2 describe-instances \
+    if [ $instance = "frontend" ]; then
+        IP=$(aws ec2 describe-instances \
             --instance-ids $INSTANCE_ID \
             --query 'Reservations[].Instances[].PublicIpAddress' \
-            --output text
-        )
+            --output text)
     else
-        IP=$(
-            aws ec2 describe-instances \
+        IP=$(aws ec2 describe-instances \
             --instance-ids $INSTANCE_ID \
             --query 'Reservations[].Instances[].PrivateIpAddress' \
-            --output text
-        )
+            --output text)
     fi
 
-    echo "IP Address :" $IP
+    echo "Instance: instance"
+    echo "IP Address:" $IP
 done
