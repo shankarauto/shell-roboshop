df5d5b87ea6b8b352236eb7d6eab0a6f68d69f70
Ravi <ravi.k@codonsystems.com>
2026-02-27 21:50:25 -0500

    actions

--- catalogue.sh
+++ catalogue.sh
@@ -35,8 +35,13 @@ VALIDATE $? "Enable NodeJS 20"
 dnf install nodejs -y &>>$LOGS_FILE
 VALIDATE $? "Install NodeJS"
 
-useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
-VALIDATE $? "Creating system user"
+id roboshop &>>$LOGS_FILE
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
+    VALIDATE $? "Creating system user"
+else
+    echo -e "Roboshop user already exists.."$Y" SKIPPING "$N
+fi    
 
 mkdir /app
 VALIDATE $? "Creating app directory"
